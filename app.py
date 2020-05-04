dd194f91ebcdfbdbd5e79e8f6e391f3fb0930cc7
Tiffany Donner <tiffany.donner@stsupport.se>
2020-05-04 12:55:29 +0000

    Testing app.py

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, url_for, request, session, redirect
+from flask.ext.pymongo import PyMongo
+import bcrypt
+
 
 app = Flask(__name__)
+
+
+app.config["MONGO_DBNAME"] = 'paw_purfect_planner'
+app.config['MONGO_URI'] = os.environ['MONGO_URI']
+app.secret_key = os.getenv("SECRET")
+
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
