bb7b963e1615dd6b62e3d3283bdce714dc3b0ef1
Tiffany Donner <tiffany.donner@stsupport.se>
2020-05-04 13:46:37 +0000

    make it better

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
-from flask import Flask, render_template, url_for, request, session, redirect
+from flask import Flask, render_template, redirect, request, url_for, request
 from flask_pymongo import PyMongo
-import bcrypt
+from bson.objectid import ObjectId
 from os import path
 if path.exists("env.py"): 
     import env
