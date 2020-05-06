6099e05044cbf3a35597f7bc53b919526ad2a5c6
Tiffany Donner <tiffany.donner@stsupport.se>
2020-05-06 11:54:36 +0000

    Deleted bootstrap theme. Also enabled edit buttons and add event

--- app.py
+++ app.py
@@ -32,6 +32,42 @@ def get_events():
     return render_template("events.html", 
                            events=mongo.db.events.find())
 
+@app.route('/add_event')
+def add_event():
+    return render_template('addevent.html',
+                           categories=mongo.db.categories.find())
+
+@app.route('/insert_event', methods=['POST'])
+def insert_event():
+    events =  mongo.db.events
+    events.insert_one(request.form.to_dict())
+    return redirect(url_for('get_events'))
+
+@app.route('/edit_event/<event_id>')
+def edit_event(event_id):
+    the_event =  mongo.db.events.find_one({"_id": ObjectId(event_id)})
+    all_categories =  mongo.db.categories.find()
+    return render_template('editevent.html', event=the_event,
+                           categories=all_categories)
+
+@app.route('/update_event/<event_id>', methods=["POST"])
+def update_event(event_id):
+    events = mongo.db.events
+    events.update( {'_id': ObjectId(event_id)},
+    {
+        'event_name':request.form.get('event_name'),
+        'category_name':request.form.get('category_name'),
+        'event_description': request.form.get('event_description'),
+        'due_date': request.form.get('due_date'),
+        'is_urgent':request.form.get('is_urgent')
+    })
+    return redirect(url_for('get_events'))
+
+@app.route('/delete_event/<event_id>')
+def delete_event(event_id):
+    mongo.db.events.remove({'_id': ObjectId(event_id)})
+    return redirect(url_for('get_events'))
+
 @app.route('/userprofile')
 def userprofile():
     
